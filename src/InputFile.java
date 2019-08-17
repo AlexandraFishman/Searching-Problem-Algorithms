ebf0d5b8439f922269007e15dcd7df7bddc6dc7c
AlexandraFishman <alexyafish@gmail.com>
2019-08-17 13:34:11 +0300

    now reading from new input file

--- src/InputFile.java
+++ src/InputFile.java
@@ -2,10 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class InputFile {
 	String algorithmToUse;
-	String  printTime;
+	String printTime;
+	boolean printOpenList;
+	LinkedList<Integer> blackTileNumber = new LinkedList<>();
+	LinkedList<Integer> redTileNumber = new LinkedList<>();
+	LinkedList<Integer> greenTileNumber = new LinkedList<>();
 	int rows;
 	int columns;
 	Integer[][] board;
@@ -17,8 +25,29 @@ public class InputFile {
 			String line = br.readLine();
 			if(line != null){
 				this.algorithmToUse = line;
+
 				line = br.readLine();
 				this.printTime = line;
+
+				line = br.readLine();
+				this.printOpenList = printOpenList(line);
+
+				line = br.readLine();
+				if(!line.equals("Black:")){
+					String [] blackTiles = line.substring(6).trim().split(",");
+					for(String s : blackTiles){
+						this.blackTileNumber.add(Integer.parseInt(s));
+					}
+				}
+
+				line = br.readLine();
+				if(!line.equals("Red:")){
+					String [] redTiles = line.substring(4).trim().split(",");
+					for(String s : redTiles){
+						this.redTileNumber.add(Integer.parseInt(s));
+					} 
+				}
+
 				line = br.readLine();
 				String boardSize = line;
 				int x = boardSize.indexOf("x");
@@ -35,33 +64,44 @@ public class InputFile {
 						}
 					}
 				}
+
+				this.endBoard(); //done for the greenTiles to be initialized
+				this.greenTileNumber.removeAll(this.blackTileNumber);
+				this.greenTileNumber.removeAll(this.redTileNumber);
+
 			}
 		} catch (IOException e) {
 			System.err.format("IOException: %s%n", e);
 		}
-		
-//		Node n = new Node(this.board.clone());
-//		BFS bfs = new BFS();
-//		AStar astr = new AStar();
-//		IDAStar idasrt = new IDAStar();
-//		DFID dfid = new DFID();
-//		Node asd = this.endBoard();
-//		long start = System.nanoTime();
-//		Node s = bfs.searchAlgorithm(n, asd);
-//		Node s = astr.searchAlgorithm(n, asd);
-//		Node s = idasrt.ida_star_algorithm(n, asd);
-//		Node s = dfid.dfid(n, asd);
-//		long elapsedTime = System.nanoTime() - start;
-//		System.out.println(elapsedTime/1e9);
-//		System.out.println("s="+s+"\n");
-//		System.out.println("wining moves: "+s.move);
-//		System.out.println("\ncost: "+s.movementCost);
-//		System.out.println("\nOriginal board:");
-//		System.out.println(this.toString()+"\n");
-//		System.out.println("Num:"+bfs.numberOfNodesCreated);
-//		System.out.println("Num:"+astr.numberOfNodesCreated);
-//		DFBnB dfbnb = new DFBnB();
-//		System.out.println(dfbnb.dfbnb_Algorithm(n, asd));
+
+		//		Node n = new Node(this.board.clone());
+		//		BFS bfs = new BFS();
+		//		AStar astr = new AStar();
+		//		IDAStar idasrt = new IDAStar();
+		//		DFID dfid = new DFID();
+		//		Node asd = this.endBoard();
+		//		long start = System.nanoTime();
+		//		Node s = bfs.searchAlgorithm(n, asd);
+		//		Node s = astr.searchAlgorithm(n, asd);
+		//		Node s = idasrt.ida_star_algorithm(n, asd);
+		//		Node s = dfid.dfid(n, asd);
+		//		long elapsedTime = System.nanoTime() - start;
+		//		System.out.println(elapsedTime/1e9);
+		//		System.out.println("s="+s+"\n");
+		//		System.out.println("wining moves: "+s.move);
+		//		System.out.println("\ncost: "+s.movementCost);
+		//		System.out.println("\nOriginal board:");
+		//		System.out.println(this.toString()+"\n");
+		//		System.out.println("Num:"+bfs.numberOfNodesCreated);
+		//		System.out.println("Num:"+astr.numberOfNodesCreated);
+		//		DFBnB dfbnb = new DFBnB();
+		//		System.out.println(dfbnb.dfbnb_Algorithm(n, asd));
+	}
+
+	private boolean printOpenList(String line) {
+		if(line.equals("no open"))
+			return false;
+		return true;
 	}
 
 	public Node endBoard() {
@@ -72,6 +112,7 @@ public class InputFile {
 			int width = this.board[0].length;
 			for (int j = 0; j < width; j++) {
 				n.stage[i][j] = d;
+				this.greenTileNumber.add(d);
 				if(d > (length*width-2)){
 					n.stage[i][j] = null;
 				}
@@ -80,12 +121,12 @@ public class InputFile {
 		}
 		return n;
 	}
-	
+
 	@Override
 	public String toString(){
 		String msg;
 		msg = this.algorithmToUse+"\n"+this.printTime+"\n"+this.columns+" "+this.rows+"\n";
-		
+
 		for(int i=0; i<rows; i++){
 			for(int j=0; j<columns; j++){
 				msg += this.board[i][j]+" ";
